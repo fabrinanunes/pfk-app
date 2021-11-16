bd11ffdb231bd5eea9379ba4cd252abc3f5ea9d8
Fabrina <84260347+fabrinanunes@users.noreply.github.com>
2021-11-16 09:48:28 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ git pull
 
 sudo pm2 stop pfk-app
 sudo npm install
-sudo npm run build
-sudo pm2 serve build/ 3000 --name "pfk-app" --spa
+# sudo npm run build
+# sudo pm2 serve build/ 3000 --name "pfk-app" --spa
+sudo pm2 start pfk-app
+sudo pm2 save
